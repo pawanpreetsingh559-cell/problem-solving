5a7ead96bb9fce68bd4b88b53da9eef31b32f954
Pawanpreet Singh <pawanpreetsingh559@gmail.com>
2026-07-01 21:17:28 +0530

    Time: 2 ms (92.02%), Space: 47.6 MB (87.76%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length==1){
+            return;
+        }
+        int insertpos=0;
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]==0){
+                i++;
+            }
+            else{
+                int temp=nums[insertpos];
+                nums[insertpos]=nums[i];
+                nums[i]=temp;
+                insertpos++;
+                i++;
+            }
+        }
+        return;
+        
+    }
+}
